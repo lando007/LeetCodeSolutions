6956be31443fc4c68e4d24a9fec164d38aaa3d6a
Luke Landon <luke.e.landon@gmail.com>
2024-01-02 15:32:07 -0800

    Adding permutations solution

---
+++ Permutations.py
@@ -0,0 +1,29 @@
+"""Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]"""
+
+from typing import List
+from itertools import permutations
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perm = permutations(nums)
+        combinedValues = list(perm)
+        return combinedValues
+
+nums = [0,1]
+
+test = Solution().permute(nums)
+print(test)
