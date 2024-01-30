8c822ca404a051b74b4dbf6a0478ebc9ad21f0c0
Luke Landon <luke.e.landon@gmail.com>
2024-01-30 12:43:53 -0800

    Addis is valid palidrome solution

---
+++ ValidPalidrome.py
@@ -0,0 +1,47 @@
+"""A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome."""
+
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sentence = re.sub(r'[^a-zA-Z0-9]', "",s)
+        sentence = sentence.replace(" ", "")
+        sentence = sentence.lower()
+        backwords = len(sentence)-1
+        print(sentence)
+        print(len(sentence))
+        if len(sentence) == 0 or len(sentence) == 1:
+            return True
+        for x in sentence:
+            if x == sentence[backwords]:
+                backwords -= 1
+                if backwords == 0:
+                    return True
+            else:
+                return False
+
+
+
+
+s = "0P"
+print(Solution().isPalindrome(s))
